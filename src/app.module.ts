e8a450f33c697132d462661707961f1d7773dbce
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2026-02-19 19:55:05 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 // El link de conexion a la base de datos es ejemplo.com:5432/mi_base_de_datos
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI!)
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
