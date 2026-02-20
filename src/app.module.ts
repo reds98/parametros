5f9336fca60f78e2c003e116c9e5bf03534d73f3
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2026-02-19 19:40:25 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 
+
+// El link de conexion a la base de datos es ejemplo.com:5432/mi_base_de_datos
 @Module({
   imports: [],
   controllers: [AppController, UsersController],
